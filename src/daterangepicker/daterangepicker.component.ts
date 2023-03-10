711b47ec689690d1e2280ec14112cfdc4aa0a65e
Adam Drozdz <adam.drozdz@iterative.pl>
2023-03-10 20:24:13 +0100

    wip

--- src/daterangepicker/daterangepicker.component.ts
+++ src/daterangepicker/daterangepicker.component.ts
@@ -982,7 +982,8 @@ export class DaterangepickerComponent implements OnInit, OnChanges {
       }
     }
     if (this.chosenLabel) {
-      this.choosedDate.emit({ chosenLabel: this.chosenLabel, startDate: this.startDate, endDate: this.endDate });
+      console.log('here :/')
+      // this.choosedDate.emit({ chosenLabel: this.chosenLabel, startDate: this.startDate, endDate: this.endDate });
     }
 
     this.datesUpdated.emit({ startDate: this.startDate, endDate: this.endDate });
